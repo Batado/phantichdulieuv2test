f0e521dae63ffd4d578e05dc9802f3ee0f0c99d6
Dat nguyn <89145165+Batado@users.noreply.github.com>
2026-04-04 08:55:59 +0700

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -8,7 +8,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-# CONFIG
 st.set_page_config(page_title="Phân tích KH - Hoa Sen", layout="wide", page_icon="📊")
 
 # UPLOAD
